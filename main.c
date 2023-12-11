f1e8e412ea6ec14c8fba3642c0d7665110314240
Claudio <wbigger@gmail.com>
2023-12-11 19:18:10 +0100

    Add tests and encrypted solution

--- main.c
+++ main.c
@@ -15,19 +15,63 @@ int main()
     /** Test */
     {
         int n = 0;
-        float l = 0.0F,p = 0.0F;
+        float l = 0.0F, p = 0.0F;
 
         l = 30;
         n = 10;
         p = calcola_perimetro(n, l);
         printf("Il perimetro con %d lati di lunghezza %0.3F e': %0.4f\n", n, l, p);
-        assert( (p > 300.0F - 0.01) && (p < 300.0F + 0.01) );
+        assert((p > 300.0F - 0.01) && (p < 300.0F + 0.01));
+        n_punti_test += 1;
+        printf("Hai completato %d punti\n", n_punti_test);
+        printf("\n");
+
+        l = 0;
+        n = 10;
+        p = calcola_perimetro(n, l);
+        printf("Il perimetro con %d lati di lunghezza %0.3F e': %0.4f\n", n, l, p);
+        assert(p==0.0F);
+        n_punti_test += 1;
+        printf("Hai completato %d punti\n", n_punti_test);
+        printf("\n");
+
+        l = -10;
+        n = 10;
+        p = calcola_perimetro(n, l);
+        printf("Il perimetro con %d lati di lunghezza %0.3F e': %0.4f\n", n, l, p);
+        assert(p==0.0F);
+        n_punti_test += 1;
+        printf("Hai completato %d punti\n", n_punti_test);
+        printf("\n");
+
+        l = 30;
+        n = 0;
+        p = calcola_perimetro(n, l);
+        printf("Il perimetro con %d lati di lunghezza %0.3F e': %0.4f\n", n, l, p);
+        assert(isnan(p));
+        n_punti_test += 1;
+        printf("Hai completato %d punti\n", n_punti_test);
+        printf("\n");
+
+        l = 30;
+        n = -10;
+        p = calcola_perimetro(n, l);
+        printf("Il perimetro con %d lati di lunghezza %0.3F e': %0.4f\n", n, l, p);
+        assert(isnan(p));
+        n_punti_test += 1;
+        printf("Hai completato %d punti\n", n_punti_test);
+        printf("\n");
+
+        l = -30;
+        n = -10;
+        p = calcola_perimetro(n, l);
+        printf("Il perimetro con %d lati di lunghezza %0.3F e': %0.4f\n", n, l, p);
+        assert(isnan(p));
         n_punti_test += 1;
         printf("Hai completato %d punti\n", n_punti_test);
         printf("\n");
     }
 
-    
     printf("Tutti i test sono andati a buon fine\n");
     printf("Punteggio finale: %d/%d\n", n_punti_test, n_punti_test);
     printf("Per il punto bonus: correzione manuale del professore\n");
